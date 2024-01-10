b6e3fbeeb54870001eaba4963b642f1df926a98e
RB <60027406+RamyaVin@users.noreply.github.com>
2024-01-10 16:13:27 +0530

    Update 5_traversal_and_basics.py

--- 5_traversal_and_basics.py
+++ 5_traversal_and_basics.py
@@ -17,7 +17,45 @@ class Solution():
 Solution.arraySum([1,2,3,2],4)
 # =============================================================================
 # =============================================================================
+# 2 Sum
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        preMap = {}
+        for i, n in enumerate(nums):
+            diff = target - n 
+            if diff in preMap:
+                return [preMap[diff],i]
+            preMap[n]=i
+        return
+
+# =============================================================================
+# =============================================================================
 # 3 Sum
+class Solution():
+    def threeSum(nums, target):
+        n=len(nums)
+        res=[]
+        nums.sort()
+        for i in range(n-2):
+            l=i+1
+            r=n-1
+            while l<r:
+                threeSum=nums[i]+nums[l]+nums[r]
+                if threeSum==target: 
+                    res.append([nums[i],nums[l],nums[r]])
+                    return res
+                elif threeSum<target:
+                    l+=1
+                else:
+                    r-=1
+            return res                  
+
+Solution.threeSum([1,2,1,5],4)
 # =============================================================================
 # =============================================================================
 # 4 Sum
