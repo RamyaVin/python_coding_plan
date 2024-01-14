b5af34308035cc8d2b6d403f3a1fb2fce8ae295d
RB <60027406+RamyaVin@users.noreply.github.com>
2024-01-14 20:13:23 +0530

    Create 9_basic_patterns_bubble_sort_kind.py

---
+++ 9_basic_patterns_bubble_sort_kind.py
@@ -0,0 +1,25 @@
+# =============================================================================
+# Basics	Bubble sort(very basic sorting technique)	https://www.geeksforgeeks.org/problems/bubble-sort/1?itm_source=geeksforgeeks&itm_medium=Article&itm_campaign=bottom_sticky_on_Article
+# =============================================================================
+# =============================================================================
+# Basic patterns	Completely understand how Node in linkedList is represented and then go ahead with the patterns	
+# =============================================================================
+# =============================================================================
+# 58	Search in a sorted array(Efficiently -> So learn binary search for this)	https://www.geeksforgeeks.org/problems/who-will-win-1587115621/1?page=1&category=Binary%20Search&sortBy=submissions
+# =============================================================================
+# =============================================================================
+# 59	Find floor and ceil in sorted array	https://www.geeksforgeeks.org/problems/floor-in-a-sorted-array-1587115620/1
+# =============================================================================
+# =============================================================================
+# 60	Find first and last occurence of element in sorted array	https://www.geeksforgeeks.org/problems/first-and-last-occurrences-of-x3116/1?page=1&category=Binary%20Search&sortBy=submissions
+# =============================================================================
+# =============================================================================
+# 61	Find missing num from 1 to N	https://leetcode.com/problems/missing-number/
+# =============================================================================
+# =============================================================================
+# 62	Find square root	https://www.geeksforgeeks.org/problems/square-root/1?itm_source=geeksforgeeks&itm_medium=Article&itm_campaign=bottom_sticky_on_Article
+# =============================================================================
+# =============================================================================
+# 63	Search for element in infinite array	https://www.codingninjas.com/studio/problems/search-in-infinite-sorted-0-1-array_696193
+# 
+# =============================================================================
