1e755b4933db67c4a89723f4df095cd91b8bb92f
RB <60027406+RamyaVin@users.noreply.github.com>
2024-01-14 20:11:40 +0530

    Create 8_Matrix Application.py

---
+++ 8_Matrix Application.py
@@ -0,0 +1,21 @@
+# =============================================================================
+# 51	Print 1 to N using recursion	https://www.geeksforgeeks.org/problems/print-1-to-n-without-using-loops-1587115620/1
+# =============================================================================
+# =============================================================================
+# 52	Factorial of N numbers	https://practice.geeksforgeeks.org/problems/factorial5739/1
+# =============================================================================
+# =============================================================================
+# 53	Fibonacci series using recursion	https://www.geeksforgeeks.org/problems/nth-fibonacci-number1335/1?itm_source=geeksforgeeks&itm_medium=Article&itm_campaign=bottom_sticky_on_Article
+# =============================================================================
+# =============================================================================
+# 54	Power(x,n) Draw the recursion tree for all the probs for sure	https://www.geeksforgeeks.org/problems/power-of-numbers-1587115620/1?itm_source=geeksforgeeks&itm_medium=Article&itm_campaign=bottom_sticky_on_Article
+# =============================================================================
+# =============================================================================
+# 55	Print pattern	https://www.geeksforgeeks.org/problems/print-pattern3549/1?page=1&category=Recursion&difficulty=Easy&sortBy=submissions
+# =============================================================================
+# =============================================================================
+# 56	Recursive implementation of atoi	https://practice.geeksforgeeks.org/problems/implement-atoi/1?utm_source=geeksforgeeks&utm_medium=ml_Article_practice_tab&utm_campaign=Article_practice_tab
+# =============================================================================
+# =============================================================================
+# 57	Pascal triangle	https://www.geeksforgeeks.org/problems/pascal-triangle0652/1?page=1&category=Recursion&difficulty=Easy&sortBy=submissions
+# =============================================================================
