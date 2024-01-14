1ffa88b003d7968b7347b3edfe21506d2e882ce4
RB <60027406+RamyaVin@users.noreply.github.com>
2024-01-14 20:14:25 +0530

    Create 10_Loop.py

---
+++ 10_Loop.py
@@ -0,0 +1,12 @@
+# =============================================================================
+# 64	Search element in sorted rotated array(With and without duplicate)	https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+# =============================================================================
+# =============================================================================
+# 65	Minimum element in sorted rotated array(With and without duplicate)A twist in normal BS is needed, once you learnt this, solve the below by yourself,even try to solve this also by yourself	https://www.geeksforgeeks.org/problems/minimum-number-in-a-sorted-rotated-array-1587115620/1?page=1&category=Binary%20Search&sortBy=submissions
+# =============================================================================
+# =============================================================================
+# 66	Number of times array is sorted	https://www.geeksforgeeks.org/problems/rotation4723/1?itm_source=geeksforgeeks&itm_medium=Article&itm_campaign=bottom_sticky_on_Article
+# =============================================================================
+# =============================================================================
+# 67	Maximum element in sorted rotated array	
+# =============================================================================
