c35eb39fc7b6013177ef0b84824e13c51c1d4324
RB <60027406+RamyaVin@users.noreply.github.com>
2024-01-15 18:01:27 +0530

    Update 11_sorting_merging_arithmetic_traversal.py

--- 11_sorting_merging_arithmetic_traversal.py
+++ 11_sorting_merging_arithmetic_traversal.py
@@ -123,11 +123,101 @@ def search(self,matrix, n, m, x):
                 if x in n:
                     return 1
         return 0
-# =============================================================================
+###
+class Solution:
+    #Function to search a given number in row-column sorted matrix.
+    def search(self,matrix, n, m, x): 
+        self.width = len(matrix[0])
+        self.matrix = matrix
+        self.x = x
+        for row in matrix:
+            if self.bisearch(row, x):
+                return 1
+        return 0
+    def bisearch(self, row, x):
+        left = 0
+        right = self.width-1
+        while left <= right:
+            midle = (left+right)//2
+            if row[midle] ==x:
+                return True
+            if row[midle] >x:
+                right = midle-1
+            else:
+                left = midle + 1
+        return False
+\# =============================================================================
 # =============================================================================
 # 72	Search in sorted matrix II	https://leetcode.com/problems/search-a-2d-matrix-ii/
+
+def searchMatrix(self, matrix, target):       
+	n = len(matrix) ## better runtime than using row and col code alone 
+	m = len(matrix[0])
+	row = 0
+	col = m - 1
+	
+	while row < n and col >= 0:
+	    if matrix[row][col] == target:
+		return True
+	    elif matrix[row][col] < target:
+		row += 1
+	    else:
+		col -= 1
+	return False
 # =============================================================================
 # =============================================================================
 # 73	Find peak in sorted matrix	https://leetcode.com/problems/find-a-peak-element-ii/
 # 
+class Solution(object):
+    def getMaxElement(self, matrix, mid):
+        index = -1
+        maxi = float('-inf')
+
+        for row in range(len(matrix)):
+            elm = matrix[row][mid]
+
+            if elm > maxi:
+                maxi = max(maxi, elm)
+                index = row
+
+        return index
+
+    def findPeakGrid(self, matrix):
+        n = len(matrix)
+        m = len(matrix[0])
+
+        start = 0
+        end = m - 1
+
+        while start <= end:
+            mid = start + (end - start) // 2
+
+            # find maximum because we have to check top, bottom, left, right
+            # if we use maximum, then our element is definitely greater than top and bottom
+            # so we need to check only left and right, i.e., our problem is reduced to finding a peak in 1D
+
+            row = self.getMaxElement(matrix, mid)
+            left = -1
+            right = -1
+
+            # handling edge cases
+            if mid - 1 >= 0:
+                left = matrix[row][mid - 1]
+
+            if mid + 1 < m:
+                right = matrix[row][mid + 1]
+
+            # we find the peak element
+            if matrix[row][mid] > left and matrix[row][mid] > right:
+                return [row, mid]
+
+            # our peak is on the left side of mid, so eliminate the right part
+            elif matrix[row][mid] > right:
+                end = mid - 1
+
+            # our peak is on the right side of mid, so eliminate the left part
+            else:
+                start = mid + 1
+
+        return [-1, -1]
 # =============================================================================
