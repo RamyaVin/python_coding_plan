b1d556aeae5bd61f3802600e39a39e4ff3ce54be
RB <60027406+RamyaVin@users.noreply.github.com>
2024-01-15 18:54:35 +0530

    Update 13_double_linked_list_sort.py

--- 13_double_linked_list_sort.py
+++ 13_double_linked_list_sort.py
@@ -1,1 +1,20 @@
 
+# =============================================================================
+# Selection sort	https://www.geeksforgeeks.org/problems/selection-sort/1?itm_source=geeksforgeeks&itm_medium=Article&itm_campaign=bottom_sticky_on_Article
+    def select(self, arr, i):
+        # code here 
+        for j in range(i,n):
+            return arr[j]
+    def selectionSort(self, arr,n):
+        for i in range(0,n):
+            for k in range(i+1,n):
+                if arr[i]>arr[k]:
+                    arr[i],arr[k]=arr[k],arr[i]
+# =============================================================================
+# =============================================================================
+# Insertion sort	https://www.geeksforgeeks.org/problems/insertion-sort/1?itm_source=geeksforgeeks&itm_medium=Article&itm_campaign=bottom_sticky_on_Article
+# =============================================================================
+# =============================================================================# Merge sort	https://www.geeksforgeeks.org/problems/merge-sort/1?itm_source=geeksforgeeks&itm_medium=Article&itm_campaign=bottom_sticky_on_Article
+# Quick sort	https://www.geeksforgeeks.org/problems/quick-sort/1?itm_source=geeksforgeeks&itm_medium=Article&itm_campaign=bottom_sticky_on_Article
+# # =============================================================================
+# =============================================================================
