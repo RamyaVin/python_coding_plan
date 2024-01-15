e26fc5adf44508a6e35f08e78987c61e1b0aa817
RB <60027406+RamyaVin@users.noreply.github.com>
2024-01-15 09:14:05 +0530

    Update 09_basic_patterns_bubble_sort_kind.py

--- 09_basic_patterns_bubble_sort_kind.py
+++ 09_basic_patterns_bubble_sort_kind.py
@@ -1,11 +1,44 @@
 # =============================================================================
 # Basics	Bubble sort(very basic sorting technique)	https://www.geeksforgeeks.org/problems/bubble-sort/1?itm_source=geeksforgeeks&itm_medium=Article&itm_campaign=bottom_sticky_on_Article
+#User function Template for python3
+
+class Solution:
+    #Function to sort the array using bubble sort algorithm.
+    def bubbleSort(self,arr, n):
+        n = len(arr)
+        for i in range(n):
+            for j in range(0, n - i - 1):
+                if arr[j] > arr[j + 1]:
+                    arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                    
+        # code here
 # =============================================================================
 # =============================================================================
 # Basic patterns	Completely understand how Node in linkedList is represented and then go ahead with the patterns	
+
+
 # =============================================================================
 # =============================================================================
 # 58	Search in a sorted array(Efficiently -> So learn binary search for this)	https://www.geeksforgeeks.org/problems/who-will-win-1587115621/1?page=1&category=Binary%20Search&sortBy=submissions
+#User function Template for python3
+
+class Solution:
+    ##Complete this function
+    def searchInSorted(self,arr, N, K):
+        #Your code here
+        l=0
+        h=N-1
+        while(l<=h):
+            mid=(l+h)//2
+            if arr[mid]==K:
+                return 1
+            elif arr[mid]>K:
+                h=mid-1
+            elif arr[mid]<K:
+                l=mid+1
+        return -1
+        
+
 # =============================================================================
 # =============================================================================
 # 59	Find floor and ceil in sorted array	https://www.geeksforgeeks.org/problems/floor-in-a-sorted-array-1587115620/1
