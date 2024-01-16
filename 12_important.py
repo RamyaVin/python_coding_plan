8144c87ac2a6191c8699900eb1c600bda75387ea
RB <60027406+RamyaVin@users.noreply.github.com>
2024-01-16 06:49:43 +0530

    Update 12_important.py

--- 12_important.py
+++ 12_important.py
@@ -27,9 +27,54 @@
 # =============================================================================
 # =============================================================================
 # 75	Allocate minimum pages	https://www.geeksforgeeks.org/problems/allocate-minimum-number-of-pages0937/1?itm_source=geeksforgeeks&itm_medium=Article&itm_campaign=bottom_sticky_on_Article
+#Function to find minimum number of pages.
+    def findPages(self,A, N, M):
+        #code here
+        if(M>N):
+            return -1
+        l = max(A)
+        h = sum(A)
+        while(l<=h):
+            m = (l+h)//2
+            if(self.stdcnt(A,m)>M):
+                l = m+1
+            else:
+                h = m-1
+        return l
+    
+    def stdcnt(self,A,pages):
+        std = 1
+        pagecnt = 0
+        for i in range(len(A)):
+            if(pagecnt+A[i]<=pages):
+                pagecnt+=A[i]
+            else:
+                std+=1
+                pagecnt = A[i]
+        return std
 # =============================================================================
 # =============================================================================
 # 76	Painter partition	https://www.geeksforgeeks.org/problems/the-painters-partition-problem1535/1?page=1&category=Binary%20Search&sortBy=submissions
+   
+    def isPossible(self, arr, n, k, mid):
+        painters_required = 1
+        current_length = 0
+        for i in range(n):
+            current_length += arr[i]
+            if current_length > mid:
+                painters_required += 1
+                current_length = arr[i]
+        return painters_required <= k
+    def minTime(self, arr, n, k):
+        low = max(arr)
+        high = sum(arr)
+        while low < high:
+            mid = (low + high) // 2
+            if self.isPossible(arr, n, k, mid):
+                high = mid
+            else:
+                low = mid + 1
+        return lo
 # =============================================================================
 # =============================================================================
 # 77	Split array largest sum	https://www.geeksforgeeks.org/problems/split-array-largest-sum--141634/1?page=2&category=Binary%20Search&sortBy=submissions
