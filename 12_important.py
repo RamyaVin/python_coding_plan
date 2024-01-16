b877b4679f2faa5937536dd0c56a973b4f74fe12
RB <60027406+RamyaVin@users.noreply.github.com>
2024-01-16 10:42:28 +0530

    Update 12_important.py

--- 12_important.py
+++ 12_important.py
@@ -295,5 +295,27 @@ print(getMedian(ar1, ar2, n1, n2))
 # =============================================================================
 # =============================================================================
 # 85	Find smallest divisor given threshold	Find the Smallest Divisor Given a Threshold - LeetCode
-# 
+
+    def smallestDivisor(self, nums, threshold):
+        low = 1
+        high = self.maxEl(nums)
+        while low <= high:
+            mid = low + (high - low) // 2
+            if self.sumByD(nums, mid) <= threshold:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return low
+        
+    def maxEl(self, nums):
+        maxi = float('-inf')
+        for num in nums:
+            maxi = max(maxi, num)
+        return maxi
+    
+    def sumByD(self, nums, div):
+        _sum = 0
+        for num in nums:
+            _sum += -(-num // div)  # Equivalent to ceil(num / div)
+        return _sum
 # =============================================================================
