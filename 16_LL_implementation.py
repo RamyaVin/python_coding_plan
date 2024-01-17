5cb953cecc6818ac66c143d5e784efccbc27e389
RB <60027406+RamyaVin@users.noreply.github.com>
2024-01-17 06:54:55 +0530

    Create 16_LL_implementation.py

---
+++ 16_LL_implementation.py
@@ -0,0 +1,115 @@
+# =============================================================================
+# Implementation probs		
+# 111	Add 1 to a number represented as LL	https://practice.geeksforgeeks.org/problems/add-1-to-a-number-represented-as-linked-list/1
+    def addOne(self,head):
+        #Returns new head of linked List.
+        string = ""
+        temp = head
+        while(temp != None):
+            string += str(temp.data)
+            temp = temp.next
+        link = LinkedList()
+        while(temp != None):
+            link.insert(temp.data)
+            temp = temp.next
+        link.insert(int(string)+1)
+        return link.head
+# =============================================================================
+# =============================================================================
+# 112	Add 2 numbers represented as LL	https://practice.geeksforgeeks.org/problems/add-two-numbers-represented-by-linked-lists/1
+
+     def addTwoLists(self, first, second):
+      # code here
+      # return head of sum list
+      f=''
+      s=''
+      while first:
+          f=f+str(first.data)
+          first=first.next
+      while second:
+          s=s+str(second.data)
+          second=second.next
+      
+      res=map(int,list(str(abs(int(f)+int(s))))) # convert string to list
+      
+      p=None
+      r=None
+      for i in res:
+          n=Node(i)
+          if p==None:
+              p=n
+              r=p
+          else:
+              p.next=n
+              p=p.next
+      return r
+# =============================================================================
+# =============================================================================
+# 113	Subtraction on LL(Comes under hard but should be solvable once the above ones are solved)	https://www.geeksforgeeks.org/problems/subtraction-in-linked-list/1?page=1&category=Linked%20List&difficulty=Hard&sortBy=submissions
+def subLinkedList(l1, l2): 
+    # Code here
+    # return head of difference list
+        #Returns new head of linked List.
+    f=''
+    s=''
+    while l1:
+        f=f+str(l1.data)
+        l1=l1.next
+    while l2:
+        s=s+str(l2.data)
+        l2=l2.next
+    
+    res=map(int,list(str(abs(int(f)-int(s))))) # convert string to list
+    
+    p=None
+    r=None
+    for i in res:
+        n=Node(i)
+        if p==None:
+            p=n
+            r=p
+        else:
+            p.next=n
+            p=p.next
+    return r
+# =============================================================================
+# =============================================================================
+# Basic pattern		
+# =============================================================================
+# =============================================================================
+# 114	Rotate LL	https://www.geeksforgeeks.org/problems/reorder-list/1?page=3&category=Linked%20List&sortBy=submissions
+# =============================================================================
+# =============================================================================
+# 115	Flattening a LL(make sure to learn the efficient approach)	https://www.geeksforgeeks.org/problems/flattening-a-linked-list--170645/1?page=4&category=Linked%20List&sortBy=submissions
+# =============================================================================
+# =============================================================================
+# 116	Delete nodes having greater value on right	https://practice.geeksforgeeks.org/problems/delete-nodes-having-greater-value-on-right/1
+# =============================================================================
+# =============================================================================
+# 117	Delete N nodes after M nodes	https://www.geeksforgeeks.org/problems/delete-n-nodes-after-m-nodes-of-a-linked-list/1?page=3&category=Linked%20List&sortBy=submissions
+# =============================================================================
+# =============================================================================
+# 118	Delete all occurence of node	
+# =============================================================================
+# =============================================================================
+# 119	Clone a LL	https://www.geeksforgeeks.org/problems/clone-a-linked-list-with-next-and-random-pointer/1?page=1&category=Linked%20List&difficulty=Hard&sortBy=submissions
+# =============================================================================
+# =============================================================================
+# 120	Length of longest palindrome in LL	https://www.geeksforgeeks.org/problems/length-of-longest-palindrome-in-linked-list/1?page=2&category=Linked%20List&difficulty=Medium&sortBy=submissions
+# =============================================================================
+# =============================================================================
+# 121	Learn the basics of circular LL	
+# =============================================================================
+# =============================================================================
+# Implementation probs		
+# =============================================================================
+# =============================================================================
+# 122	Learn the basic representation of the nodes in LL	
+# =============================================================================
+# =============================================================================
+# 123	Insert, Delete, Reverse DLL	https://www.geeksforgeeks.org/problems/reverse-a-doubly-linked-list/1?page=1&category=doubly-linked-list&sortBy=submissions
+# =============================================================================
+# =============================================================================
+# 124	Pairs with given sum in DLL	https://www.geeksforgeeks.org/problems/find-pairs-with-given-sum-in-doubly-linked-list/1?page=1&category=doubly-linked-list&difficulty=Easy&sortBy=submissions
+# =============================================================================
+# =============================================================================
