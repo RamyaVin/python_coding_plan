ff471fc0102b5f394efa1c06af34e1c3f0a0a1f0
RB <60027406+RamyaVin@users.noreply.github.com>
2024-02-04 18:08:45 +0530

    Create 30_Greedy - Technique.py

---
+++ 30_Greedy - Technique.py
@@ -0,0 +1,36 @@
+
+# =============================================================================
+# =============================================================================
+# 30_Greedy - Technique		
+# 418	Minimum cost to connect ropes	https://practice.geeksforgeeks.org/problems/minimum-cost-of-ropes-1587115620/1
+# 419	Meeting room II	https://leetcode.com/problems/meeting-rooms-ii
+# 420	Minimum cost to hire k workers	https://leetcode.com/problems/minimum-cost-to-hire-k-workers/
+# 421	Minimum num of CPU	https://leetcode.com/problems/task-scheduler/
+# 422	Minimum num of refueling stops	https://leetcode.com/problems/minimum-number-of-refueling-stops/
+# Dynamic Programming - Technique	DP is super crucial topic and very commonly asked in many interviews!! One major thing you need to know is, dont start with tabulation approach directly, thats wrong way to learn DP. First find the solution using recursion, then memoization and then finally tabulation and space reduction in tabulation.	
+# Heaps - Data structure		
+# 423	Implement Trie from scratch	https://www.codingninjas.com/studio/problems/implement-trie_631356
+# 424	Word break (Do solve it using trie, though you had solved this already)	https://www.geeksforgeeks.org/problems/word-break-trie--141631/1?itm_source=geeksforgeeks&itm_medium=Article&itm_campaign=bottom_sticky_on_Article
+# 425	Longest string with all prefixes	https://leetcode.com/problems/longest-common-prefix/
+# 426	Maximum XOR of two numbers in array	https://www.geeksforgeeks.org/problems/maximum-xor-of-two-numbers-in-an-array/1?itm_source=geeksforgeeks&itm_medium=Article&itm_campaign=bottom_sticky_on_Article
+# 427	Unique rows in boolean matrix	https://www.geeksforgeeks.org/problems/unique-rows-in-boolean-matrix/1?itm_source=geeksforgeeks&itm_medium=Article&itm_campaign=bottom_sticky_on_Article
+# 428	Prefix match with other strings	https://www.geeksforgeeks.org/problems/prefix-match-with-other-strings/1
+# Trie - Data structure		
+# String	Most of the imp problems in strings involves diff techniques, algos which are already covered. In this section, we will focus on string specific imp probs and algos	
+# 429	Implement Atoi	https://www.geeksforgeeks.org/problems/implement-atoi/1
+# 430	Count and say	https://leetcode.com/problems/count-and-say/
+# 431	Minimum bracket reversals	https://www.geeksforgeeks.org/problems/count-the-reversals0401/1?itm_source=geeksforgeeks&itm_medium=Article&itm_campaign=bottom_sticky_on_Article
+# 432	Rabin karp algo( Pattern searching, super imp, this was infact asked in one of my interview)	https://practice.geeksforgeeks.org/problems/search-pattern-rabin-karp-algorithm--141631/1
+# 433	Boyer moor algo (Pattern searching)	https://www.codingninjas.com/studio/problems/boyer-moore-algorithm-for-pattern-searching_1115634
+# 434	KMP algo ( Longest prefix suffix, super imp)	https://practice.geeksforgeeks.org/problems/longest-prefix-suffix2527/1
+# 435	Longest happy prefix	https://www.geeksforgeeks.org/longest-prefix-also-suffix/
+# 436	Remove all adjacent duplicates	https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
+# 437	Hashing in strings	https://www.geeksforgeeks.org/practice-problems-on-hashing/
+# 438	Print all anagrams	https://leetcode.com/problems/group-anagrams/
+# 439	Z - Function	https://practice.geeksforgeeks.org/problems/search-pattern-z-algorithm--141631/1
+# 440	Shortest palindrome	Shortest Palindrome - LeetCode
+# 441	Circle of strings	https://www.geeksforgeeks.org/problems/circle-of-strings4530/1
+# 442	Roman numerals to decimals	https://www.geeksforgeeks.org/problems/roman-number-to-integer3201/1?itm_source=geeksforgeeks&itm_medium=Article&itm_campaign=bottom_sticky_on_Article
+# 
+# =============================================================================
+
