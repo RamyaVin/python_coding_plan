f4c8220356bd22e44eb787aff2f440f0c2d466ad
RB <60027406+RamyaVin@users.noreply.github.com>
2024-02-04 18:04:13 +0530

    Create # 23_Must solve imp algos.py

---
+++ # 23_Must solve imp algos.py
@@ -0,0 +1,19 @@
+# =============================================================================
+# =============================================================================
+# 23_Must solve imp algos		
+# 211	Insert a node in BST	https://leetcode.com/problems/insert-into-a-binary-search-tree/
+# 212	Search a value in BST(Learn efficient algo to do it, since this can be handled efficiently in BST than BT)	https://practice.geeksforgeeks.org/problems/search-a-node-in-bst/1
+# 213	Find minimum and maximum in BST	https://www.geeksforgeeks.org/problems/max-and-min-element-in-binary-tree/1?page=5&category=Tree&sortBy=submissions
+# 214	Find the Kth largest element/kth Smallest in BST(Try to solve it without looking for soln, once you have mastered all the above patterns, this should be easy)	https://www.geeksforgeeks.org/problems/kth-largest-element-in-bst/1?page=2&category=Tree&sortBy=submissions
+# 215	Check for BST(Try solving probs in this pattern by yourself till LCA once above are solved)	https://www.geeksforgeeks.org/problems/check-for-bst/1?page=1&category=Tree&sortBy=submissions
+# 216	Find the closest element in BST	https://www.geeksforgeeks.org/problems/find-the-closest-element-in-bst/1?page=3&category=Tree&sortBy=submissions
+# 217	Count BST nodes in the given range	https://www.geeksforgeeks.org/problems/count-bst-nodes-that-lie-in-a-given-range/1?page=3&category=Tree&sortBy=submissions
+# 218	Largest BST in BT(Super imp - Must solve)	https://www.geeksforgeeks.org/problems/largest-bst/1?page=2&category=Tree&sortBy=submissions
+# 219	Lowest Common Ancestor in BST	https://www.geeksforgeeks.org/problems/lowest-common-ancestor-in-a-bst/1?page=1&category=Tree&sortBy=submissions
+# 220	Merge two BST(Super imp - Must solve)	https://www.geeksforgeeks.org/problems/merge-two-bst-s/1?page=4&category=Tree&sortBy=submissions
+# 221	Inorder successor and predecessor	https://www.geeksforgeeks.org/problems/predecessor-and-successor/1?page=2&category=Tree&sortBy=submissions
+# 222	Populate inorder successor for all nodes	https://www.geeksforgeeks.org/problems/populate-inorder-successor-for-all-nodes/1?page=5&category=Tree&sortBy=submissions
+# 223	BST to greater sum tree	https://www.geeksforgeeks.org/problems/bst-to-greater-sum-tree/1?page=8&category=Tree&sortBy=submissions
+# 224	Delete given node from BST	https://leetcode.com/problems/delete-node-in-a-bst/
+# =============================================================================
+# =============================================================================
