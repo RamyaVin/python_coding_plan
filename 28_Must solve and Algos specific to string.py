b74cd09ffd630ecb210209f8be06ed81bc9cc8d6
RB <60027406+RamyaVin@users.noreply.github.com>
2024-02-04 18:06:47 +0530

    Create 28_Must solve and Algos specific to string.py

---
+++ 28_Must solve and Algos specific to string.py
@@ -0,0 +1,64 @@
+# =============================================================================
+# =============================================================================
+# 28_Must solve and Algos specific to string		
+# Basics		
+# 333	Understand the basics!!	
+# Array - Data structure	This is a huge topic and array acts as base data structure for many concepts, so lets master the basics in this section. For all the data struture moving forward, make sure to visually note down and understand how it is represented	
+# 334	Count num of hops	https://www.geeksforgeeks.org/problems/count-number-of-hops-1587115620/1?page=2&category=Dynamic%20Programming&sortBy=submissions
+# 335	Count way to reach nth stair(Order matters)	https://www.geeksforgeeks.org/problems/count-ways-to-reach-the-nth-stair-1587115620/1?page=2&category=Dynamic%20Programming&sortBy=submissions
+# 336	Count way to reach nth stair(Order doesn't matter)	https://www.geeksforgeeks.org/problems/count-ways-to-nth-stairorder-does-not-matter5639/1?page=4&category=Dynamic%20Programming&sortBy=submissions
+# 337	House robber	https://leetcode.com/problems/house-robber/
+# 338	House robber II	https://leetcode.com/problems/house-robber-ii/
+# Time and space complexity	https://youtu.be/FPu9Uld7W-E?si=s_d6cjdb6UtlVg16 - After learning this, make sure to learn time and space complexity for all the problems moving forward	
+# 339	Count paths - grid	https://www.geeksforgeeks.org/problems/count-the-paths4332/1?page=5&category=Dynamic%20Programming&sortBy=submissions
+# 340	Unique paths II	https://leetcode.com/problems/unique-paths-ii/
+# 341	Count all possible paths with condition(You can solve this by yourself once above two are solved)	https://www.geeksforgeeks.org/problems/count-all-possible-paths-from-top-left-to-bottom-right3011/1?page=3&category=Dynamic%20Programming&sortBy=submissions
+# 342	Minimum path sum in grid	https://leetcode.com/problems/minimum-path-sum/
+# 343	Max path sum 	https://www.geeksforgeeks.org/problems/path-in-matrix3805/1?page=2&category=Dynamic%20Programming&sortBy=submissions
+# 344	Minimum path sum in triangular grid	https://leetcode.com/problems/triangle/
+# 345	Number of paths with k coins	https://www.geeksforgeeks.org/problems/number-of-paths-in-a-matrix-with-k-coins2728/1?page=7&category=Dynamic%20Programming&sortBy=submissions
+# 346	Minimum/Maximum falling path sum	https://leetcode.com/problems/minimum-falling-path-sum
+# 347	Minimum falling path sum II	https://leetcode.com/problems/minimum-falling-path-sum-ii
+# Matrix - Data structure	Once you learn this, in many advanced topics like graphs, DP, backtracking, this is super imp. This section is to learn 2D array and to get grip on basics	
+# 348	Subset sum problem	https://www.geeksforgeeks.org/problems/subset-sum-problem-1611555638/1?page=1&category=Dynamic%20Programming&status=solved&sortBy=submissions
+# 349	Count subset with given sum k	https://www.geeksforgeeks.org/problems/perfect-sum-problem5633/1?page=1&category=Dynamic%20Programming&sortBy=submissions
+# 350	Partition equal subset sum(Try to do it on own after solving above two, just a small trick)	https://www.geeksforgeeks.org/problems/subset-sum-problem2014/1?page=1&category=Dynamic%20Programming&sortBy=submissions
+# 351	Minimum sum partition	https://www.geeksforgeeks.org/problems/minimum-sum-partition3317/1?page=2&category=Dynamic%20Programming&sortBy=submissions
+# 352	Count partition with given diff	
+# 353	0-1 Knapsack	https://www.geeksforgeeks.org/problems/0-1-knapsack-problem0945/1?page=1&category=Dynamic%20Programming&sortBy=submissions
+# 354	Maximum non adjacent subsequence	https://www.geeksforgeeks.org/problems/max-sum-without-adjacents2430/1?page=3&category=Dynamic%20Programming&sortBy=submissions
+# 355	Coin change	https://www.geeksforgeeks.org/problems/coin-change2448/1?page=1&category=Dynamic%20Programming&sortBy=submissions
+# 356	Min number of coins	https://www.geeksforgeeks.org/problems/number-of-coins1824/1?page=2&category=Dynamic%20Programming&sortBy=submissions
+# 357	Rod cutting	https://www.geeksforgeeks.org/problems/rod-cutting0840/1?page=2&category=Dynamic%20Programming&status=solved&sortBy=submissions
+# 358	Target sum	https://leetcode.com/problems/target-sum
+# 359	Minimum cost to fill given weight in bag(Solving the above probs in this pattern will make this super easy)	 https://www.geeksforgeeks.org/problems/minimum-cost-to-fill-given-weight-in-a-bag1956/1?page=3&category=Dynamic%20Programming&status=solved&sortBy=submissions
+# 360	Knapsack with duplication items	https://www.geeksforgeeks.org/problems/knapsack-with-duplicate-items4201/1?page=2&category=Dynamic%20Programming&sortBy=submissions
+# Recursion - Technique	Recursion is super important concept!! Lets first understand the basics of recursion in this section before moving forward	
+# 361	Longest increasing subsequence	https://leetcode.com/problems/longest-increasing-subsequence/
+# 362	Print longest increasing subsequence	
+# 363	Longest bitonic subsequence(Solve on own, definitely you can do it once the above two are solved)	https://www.geeksforgeeks.org/problems/longest-bitonic-subsequence0824/1?page=4&category=Dynamic%20Programming&sortBy=submissions
+# 364	Longest divisible subset(This and the below are just extended version of longest increasing subsequence, so try on own	https://leetcode.com/problems/largest-divisible-subset/
+# 365	Longest string chain	https://leetcode.com/problems/longest-string-chain/
+# 366	Num of longest increasing subsequence	https://leetcode.com/problems/number-of-longest-increasing-subsequence/
+# Binary Search - Algo		
+# 367	Longest common subsequence(Once you learn this pattern, all probs under this pattern can be solved by yourself)	https://www.geeksforgeeks.org/problems/longest-common-subsequence-1587115620/1?page=1&category=Dynamic%20Programming&sortBy=submissions
+# 368	Print Longest common subsequence(Here you will need to print the string so must solve)	https://www.geeksforgeeks.org/problems/print-all-lcs-sequences3413/1?page=7&category=Dynamic%20Programming&sortBy=submissions
+# 369	Longest palindromic subsequence	https://leetcode.com/problems/longest-palindromic-subsequence
+# 370	Form a palindrome with minimum insertions	https://leetcode.com/problems/minimum-insertion-steps-to-make-a-string-palindrome
+# 371	Min num of deletions and insertions to make string s1 equal to s2	https://www.geeksforgeeks.org/problems/minimum-number-of-deletions-and-insertions0209/1?page=4&category=Dynamic%20Programming&sortBy=submissions
+# 372	Shortest common supersequence	https://www.geeksforgeeks.org/problems/shortest-common-supersequence0322/1?page=2&category=Dynamic%20Programming&sortBy=submissions
+# 373	Longest common substring/subarray	https://www.geeksforgeeks.org/problems/longest-common-substring1452/1?page=1&category=Dynamic%20Programming&sortBy=submissions
+# 374	Longest palindromic substring	https://www.geeksforgeeks.org/problems/longest-palindrome-in-a-string3411/1?page=1&category=Dynamic%20Programming&sortBy=submissions
+# 375	Longest repeating subsequence	https://www.geeksforgeeks.org/problems/longest-repeating-subsequence2004/1?page=2&category=Dynamic%20Programming&status=solved&sortBy=submissions
+# Sorting - Algo	Make sure to completely understand the algo thoroughly, and time & space complexity	
+# 376	Edit distance (All the probs in this pattern are super imp, each prob is different so do solve all)	https://www.geeksforgeeks.org/problems/edit-distance3702/1?page=1&category=Dynamic%20Programming&sortBy=submissions
+# 377	Distinct occurence	https://www.geeksforgeeks.org/problems/distinct-occurrences/1?page=3&category=Dynamic%20Programming&status=solved&sortBy=submissions
+# 378	Wildcard pattern matching	https://www.geeksforgeeks.org/problems/wildcard-pattern-matching/1?page=4&category=Dynamic%20Programming&sortBy=submissions
+# 379	Longest valid parentheses	https://www.geeksforgeeks.org/problems/longest-valid-parentheses5657/1?page=3&category=Dynamic%20Programming&sortBy=submissions
+# 380	Word break	https://www.geeksforgeeks.org/problems/word-break1352/1?page=3&category=Dynamic%20Programming&sortBy=submissions
+# 381	Word break II	https://www.geeksforgeeks.org/problems/word-break-part-23249/1?page=5&category=Dynamic%20Programming&sortBy=submissions
+# 382	Word wrap	https://www.geeksforgeeks.org/problems/word-wrap1646/1?page=4&category=Dynamic%20Programming&sortBy=submissions
+# 383	Interleaved strings	https://www.geeksforgeeks.org/problems/interleaved-strings/1?page=3&category=Dynamic%20Programming&sortBy=submissions
+
+# =============================================================================
+# =============================================================================
