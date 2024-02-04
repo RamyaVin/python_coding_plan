53e3d4059b784d3925caaa9649ab42da7d5856d8
RB <60027406+RamyaVin@users.noreply.github.com>
2024-02-04 18:02:45 +0530

    Create 20_Topological sort.py

---
+++ 20_Topological sort.py
@@ -0,0 +1,18 @@
+# =============================================================================
+# =============================================================================
+#20_Topological sort		
+# Multi Source		
+# Topological sort		
+# 180	PreOrder Traversal	https://www.geeksforgeeks.org/problems/preorder-traversal/1?page=1&category=Tree&sortBy=submissions
+# 181	InOrder Traversal	https://www.geeksforgeeks.org/problems/inorder-traversal/1?page=2&category=Tree&sortBy=submissions
+# 182	PostOrder Traversal	https://www.geeksforgeeks.org/problems/postorder-traversal/1?page=2&category=Tree&sortBy=submissions
+# 183	Level Order Traversal	https://www.geeksforgeeks.org/problems/level-order-traversal/1?page=1&category=Tree&sortBy=submissions
+# 184	Boundary Traversal(Once level traversal is learnt, this you can solve)	https://www.geeksforgeeks.org/problems/boundary-traversal-of-binary-tree/1?page=1&category=Tree&sortBy=submissions
+# 185	Vertical Traversal(Once level traversal is learnt, this you can solve, lil extra logic is needed)	https://www.geeksforgeeks.org/problems/print-a-binary-tree-in-vertical-order/1?page=1&category=Tree&sortBy=submissions
+# 186	Top View of BT(Once you have mastered level traversal, this should be easy)	https://www.geeksforgeeks.org/problems/top-view-of-binary-tree/1?page=1&category=Tree&sortBy=submissions
+# 187	Bottom View of BT(Once you have mastered level traversal, this should be easy)	https://www.geeksforgeeks.org/problems/bottom-view-of-binary-tree/1?page=1&category=Tree&sortBy=submissions
+# 188	Left View of BT(Once you have mastered vertical traversal, this should be easy)	https://www.geeksforgeeks.org/problems/left-view-of-binary-tree/1?page=1&category=Tree&sortBy=submissions
+# 189	Right View of BT(Once you have mastered vertical traversal, this should be easy)	https://www.geeksforgeeks.org/problems/right-view-of-binary-tree/1?page=1&category=Tree&sortBy=submissions
+# 190	Diagonal Traversal	https://www.geeksforgeeks.org/problems/diagonal-traversal-of-binary-tree/1?page=3&category=Tree&sortBy=submissions
+# =============================================================================
+# =============================================================================
