ef8734f63b0babb5590f7de688973f0629e7f620
RB <60027406+RamyaVin@users.noreply.github.com>
2024-02-04 18:03:17 +0530

    Create 21_Minimum Spanning Tree.py

---
+++ 21_Minimum Spanning Tree.py
@@ -0,0 +1,16 @@
+# =============================================================================
+# =============================================================================
+# 21_Minimum Spanning Tree		
+# 191	Insert node in a tree	
+# 192	Height of the tree	https://www.geeksforgeeks.org/problems/height-of-binary-tree/1?page=1&category=Tree&sortBy=submissions
+# 193	Diameter of the tree	https://www.geeksforgeeks.org/problems/diameter-of-binary-tree/1?page=1&category=Tree&sortBy=submissions
+# 194	Check if 2 trees are identical	https://www.geeksforgeeks.org/problems/determine-if-two-trees-are-identical/1?page=1&category=Tree&sortBy=submissions
+# 195	Check if subtree	https://www.geeksforgeeks.org/problems/check-if-subtree/1?page=2&category=Tree&sortBy=submissions
+# 196	Check for balanced tree	https://www.geeksforgeeks.org/problems/check-for-balanced-tree/1?page=1&category=Tree&sortBy=submissions
+# 197	Lowest common Ancestor in BT	https://www.geeksforgeeks.org/problems/lowest-common-ancestor-in-a-binary-tree/1?page=1&category=Tree&sortBy=submissions
+# 198	Sum tree	https://www.geeksforgeeks.org/problems/sum-tree/1?page=1&category=Tree&sortBy=submissions
+# 199	Symmetric tree	https://www.geeksforgeeks.org/problems/symmetric-tree/1?page=2&category=Tree&sortBy=submissions
+# 200	Mirror of a tree	https://www.geeksforgeeks.org/problems/mirror-tree/1?page=1&category=Tree&sortBy=submissions
+# 201	Check if isomorphic	https://www.geeksforgeeks.org/problems/check-if-tree-is-isomorphic/1?page=2&category=Tree&sortBy=submissions
+# =============================================================================
+# =============================================================================
