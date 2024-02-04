94ba25bfe43bc3f40ea54f1473296bdf108c45ac
RB <60027406+RamyaVin@users.noreply.github.com>
2024-02-04 18:05:15 +0530

    Create 25_DP on subsequence.py

---
+++ 25_DP on subsequence.py
@@ -0,0 +1,37 @@
+# =============================================================================
+# =============================================================================
+# 25_DP on subsequence		
+# DP on Increasing subsequence		
+# 258	BFS Traversal(Super imp and basic)	https://www.geeksforgeeks.org/problems/bfs-traversal-of-graph/1?page=1&category=Graph&sortBy=submissions
+# 259	DFS Traversal(Super imp and basic)	https://www.geeksforgeeks.org/problems/depth-first-traversal-for-a-graph/1?page=1&category=Graph&sortBy=submissions
+# 260	Detect cycle in Directed graph	https://www.geeksforgeeks.org/problems/detect-cycle-in-a-directed-graph/1?page=1&category=Graph&sortBy=submissions
+# 261	Detect cycle in Undirected graph	https://www.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1?page=1&category=Graph&sortBy=submissions
+# 262	Detect Negative cycle in graph	https://www.geeksforgeeks.org/problems/negative-weight-cycle3504/1?page=2&category=Graph&sortBy=submissions
+# DP on strings(Using Longest common subsequence pattern)		
+# 263	Connected component(Once you master this , all the below using this pattern will become super easy)	https://practice.geeksforgeeks.org/problems/strongly-connected-component-tarjanss-algo-1587115621/1
+# 264	Number of Provinces(Learn how to solve this prob and solve the below in this pattern)	https://www.geeksforgeeks.org/problems/number-of-provinces/1?page=2&category=Graph&sortBy=submissions
+# 265	Number of Islands	https://www.geeksforgeeks.org/problems/find-the-number-of-islands/1?page=1&category=Graph&sortBy=submissions
+# 266	Number of distinct island	https://www.geeksforgeeks.org/problems/number-of-distinct-islands/1?page=2&category=Graph&sortBy=submissions
+# 267	Number of closed island	https://www.geeksforgeeks.org/problems/find-number-of-closed-islands/1?page=3&category=Graph&sortBy=submissions
+# DP on string	 	
+# 268	Bipartite graph	https://www.geeksforgeeks.org/problems/bipartite-graph/1?page=1&category=Graph&sortBy=submissions
+# 269	M-colouring	https://practice.geeksforgeeks.org/problems/m-coloring-problem-1587115620/1
+# DP on stocks(Was asked in one of my interview too, so dont miss any ques from this pattern)		
+# 270	Find shortest path in DAG	https://www.geeksforgeeks.org/problems/shortest-path-in-undirected-graph/1?page=1&category=Graph&sortBy=submissions
+# 271	Djikstra Algo	https://www.geeksforgeeks.org/problems/implementing-dijkstra-set-1-adjacency-matrix/1?page=1&category=Graph&sortBy=submissions
+# 272	Step by Knight	https://www.geeksforgeeks.org/problems/steps-by-knight5927/1?page=1&category=Graph&sortBy=submissions
+# 273	Shortest path source to destiny	https://www.geeksforgeeks.org/problems/shortest-source-to-destination-path3544/1?page=1&category=Graph&sortBy=submissions
+# 274	Find if path exists	https://www.geeksforgeeks.org/problems/find-whether-path-exist5238/1?page=2&category=Graph&sortBy=submissions
+# 275	Covid spread	https://www.geeksforgeeks.org/problems/covid-spread--141631/1?page=4&category=Graph&sortBy=submissions
+# 276	Minimum cost path	https://www.geeksforgeeks.org/problems/minimum-cost-path3833/1?page=2&category=Graph&sortBy=submissions
+# 277	Word ladder I	https://www.geeksforgeeks.org/problems/word-ladder/1?page=3&category=Graph&sortBy=submissions
+# 278	Word ladder II	https://www.geeksforgeeks.org/problems/word-ladder-ii/1?page=3&category=Graph&sortBy=submissions
+# 279	Floyd warshal algo	https://www.geeksforgeeks.org/problems/implementing-floyd-warshall2042/1?page=1&category=Graph&sortBy=submissions
+# 280	Bellmanford algo(Imp - Will be used for negative cycle)	https://www.geeksforgeeks.org/problems/distance-from-the-source-bellman-ford-algorithm/1?page=2&category=Graph&sortBy=submissions
+# DP on partition and MCM		
+# 281	Flood fill	https://www.geeksforgeeks.org/problems/flood-fill-algorithm1856/1?page=1&category=Graph&sortBy=submissions
+# 282	Replace O's with X's(Below can be solved easily in this pattern once you learnt flood fill)	https://www.geeksforgeeks.org/problems/replace-os-with-xs0052/1?page=1&category=Graph&sortBy=submissions
+# 283	Unit area of largest region of 1s	https://www.geeksforgeeks.org/problems/length-of-largest-region-of-1s-1587115620/1?page=2&category=Graph&sortBy=submissions
+# 284	Rotten Oranges	https://www.geeksforgeeks.org/problems/rotten-oranges2536/1?page=1&category=Graph&sortBy=submissions
+# =============================================================================
+# =============================================================================
