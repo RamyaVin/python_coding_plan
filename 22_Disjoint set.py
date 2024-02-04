b2434457df6f5f8e6b61c9eab4fcf107f8c3c28e
RB <60027406+RamyaVin@users.noreply.github.com>
2024-02-04 18:03:49 +0530

    Create 22_Disjoint set.py

---
+++ 22_Disjoint set.py
@@ -0,0 +1,14 @@
+# =============================================================================
+# =============================================================================
+# 22_Disjoint set		
+# 202	Root to leaf paths	https://www.geeksforgeeks.org/problems/root-to-leaf-paths/1?page=4&category=Tree&sortBy=submissions
+# 203	Root to leaf path sum (Once the root to leaf path is solved, this should be easy)	https://www.geeksforgeeks.org/problems/root-to-leaf-paths/1?page=4&category=Tree&sortBy=submissions
+# 204	Maximum path sum from any node(Once the root to leaf path is solved, this should be easy)	https://www.geeksforgeeks.org/problems/maximum-path-sum-from-any-node/1?page=3&category=Tree&sortBy=submissions
+# 205	K Sum Paths (Once the root to leaf path is solved, this should be easy)	https://www.geeksforgeeks.org/problems/k-sum-paths/1?page=3&category=Tree&sortBy=submissions
+# 206	Nodes at given distance	https://www.geeksforgeeks.org/problems/nodes-at-given-distance-in-binary-tree/1?page=3&category=Tree&sortBy=submissions
+# 207	Range sum of BST(Solving nodes at given distance, will make this easy)	
+# 208	Minimum distance between 2 nodes(Solving nodes at given distance, will make this easy)	https://www.geeksforgeeks.org/problems/min-distance-between-two-given-nodes-of-a-binary-tree/1?page=2&category=Tree&sortBy=submissions
+# 209	Maximum distance between node and ancestor(Must solve though distance pattern is covered)	https://www.geeksforgeeks.org/problems/maximum-difference-between-node-and-its-ancestor/1?page=3&category=Tree&sortBy=submissions
+# 210	Min time to burn a tree(Using distance logic, this can be sovled)	https://www.geeksforgeeks.org/problems/burning-tree/1?page=4&category=Tree&sortBy=submissions
+# =============================================================================
+# =============================================================================
