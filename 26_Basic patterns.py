72beec939b39a0bd23eadcef93a501f5a7de08ef
RB <60027406+RamyaVin@users.noreply.github.com>
2024-02-05 11:16:25 +0530

    Update 26_Basic patterns.py

--- 26_Basic patterns.py
+++ 26_Basic patterns.py
@@ -2,33 +2,89 @@
 # =============================================================================
 # 26_Basic patterns		
 # 285	Topological sort algo(Learning this algo, will make you solve all the probs in this pattern)	https://www.geeksforgeeks.org/problems/topological-sort/1?page=1&category=Graph&sortBy=submissions
+# =============================================================================
+# =============================================================================
 # 286	Prerequisite tasks	https://www.geeksforgeeks.org/problems/prerequisite-tasks/1?page=2&category=Graph&sortBy=submissions
+# =============================================================================
+# =============================================================================
 # 287	Course schedule I	https://www.geeksforgeeks.org/problems/course-schedule/1?page=3&category=Graph&sortBy=submissions
+# =============================================================================
+# =============================================================================
 # 288	Course schedule II	https://leetcode.com/problems/course-schedule-ii
+# =============================================================================
+# =============================================================================
 # 289	Eventual safe states	https://www.geeksforgeeks.org/problems/eventual-safe-states/1?page=2&category=Graph&sortBy=submissions
+# =============================================================================
+# =============================================================================
 # 290	Alien dictionary	https://www.geeksforgeeks.org/problems/alien-dictionary/1?page=1&category=Graph&sortBy=submissions
+# =============================================================================
+# =============================================================================
 # Top k pattern		
+# =============================================================================
+# =============================================================================
 # 291	Minimum spanning tree(Once you learn this, then you will be able to solve the probs in this pattern by yourself)	https://www.geeksforgeeks.org/problems/minimum-spanning-tree/1?page=1&category=Graph&sortBy=submissions
+# =============================================================================
+# =============================================================================
 # 292	Kruskals algo	https://www.codingninjas.com/studio/problems/code-kruskal-s-algorithm_6320
+# =============================================================================
+# =============================================================================
 # 293	Minimum cost to connect cities	https://leetcode.com/problems/connecting-cities-with-minimum-cost
+# =============================================================================
+# =============================================================================
 # 294	Minimum cost to connect all points	https://leetcode.com/problems/min-cost-to-connect-all-points/
+# =============================================================================
+# =============================================================================
 # 295	Optimize water distribution	https://leetcode.com/problems/optimize-water-distribution-in-a-village
+# =============================================================================
+# =============================================================================
 # Merge k sorted pattern		
+# =============================================================================
+# =============================================================================
 # 296	Learn the disjoint set Algo	
+# =============================================================================
+# =============================================================================
 # 297	Union by rank, path compression(Super imp, dont skip)	
+# =============================================================================
+# =============================================================================
 # 298	Networks connected	https://leetcode.com/problems/number-of-operations-to-make-network-connected/
+# =============================================================================
+# =============================================================================
 # 299	Minimum swaps to sort	https://www.geeksforgeeks.org/problems/minimum-swaps/1?page=1&category=Graph&sortBy=submissions
+# =============================================================================
+# =============================================================================
 # 300	Santa Banta	https://www.geeksforgeeks.org/problems/santa-banta2814/1?page=3&category=Graph&sortBy=submissions
+# =============================================================================
+# =============================================================================
 # 301	Number of pairs	https://www.geeksforgeeks.org/problems/number-of-pairs-1645358985/1?page=5&category=Graph&sortBy=submissions
+# =============================================================================
+# =============================================================================
 # 302	Number of Island II	https://leetcode.com/problems/number-of-islands-ii/
+# =============================================================================
+# =============================================================================
 # 303	Making a large island	https://leetcode.com/problems/making-a-large-island
+# =============================================================================
+# =============================================================================
 # Two heaps pattern		
+# =============================================================================
+# =============================================================================
 # 304	Bridges in graph	https://www.geeksforgeeks.org/problems/bridge-edge-in-graph/1?page=3&category=Graph&sortBy=submissions
+# =============================================================================
+# =============================================================================
 # 305	Articulation point	https://www.geeksforgeeks.org/problems/articulation-point-1/1?page=3&category=Graph&sortBy=submissions
+# =============================================================================
+# =============================================================================
 # 306	Critical connection	https://www.geeksforgeeks.org/problems/critical-connections/1?page=5&category=Graph&sortBy=submissions
+# =============================================================================
+# =============================================================================
 # 307	Kosaraja algo (Strongly connected component algo)	https://www.geeksforgeeks.org/problems/strongly-connected-components-kosarajus-algo/1?page=2&category=Graph&sortBy=submissions
+# =============================================================================
+# =============================================================================
 # 308	Word boggle	https://www.geeksforgeeks.org/problems/word-boggle4143/1?page=2&category=Graph&sortBy=submissions
+# =============================================================================
+# =============================================================================
 # 309	Word boggle II	https://www.geeksforgeeks.org/problems/word-boggle-ii--141631/1?page=5&category=Graph&sortBy=submissions
+# =============================================================================
+# =============================================================================
 # Minimum number pattern	
 # =============================================================================
 # =============================================================================	
