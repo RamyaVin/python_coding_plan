d76de86940a7103f9cdcf259fad77b464c848f3b
RB <60027406+RamyaVin@users.noreply.github.com>
2024-02-05 11:17:16 +0530

    Update 27_Basic and must solve.py

--- 27_Basic and must solve.py
+++ 27_Basic and must solve.py
@@ -1,28 +1,74 @@
 # =============================================================================
 # =============================================================================	
 # 27_Basic and must solve		
+# =============================================================================
+# =============================================================================
 # 310	Minimum number of jumps	https://www.geeksforgeeks.org/problems/minimum-number-of-jumps-1587115620/1?page=1&category=Greedy&sortBy=submissions
+# =============================================================================
+# =============================================================================
 # 311	Minimum operations	https://www.geeksforgeeks.org/problems/find-optimum-operation4504/1?page=1&category=Greedy&sortBy=submissions
+# =============================================================================
+# =============================================================================
 # 312	Minimize the sum of product	https://www.geeksforgeeks.org/problems/minimize-the-sum-of-product1525/1?page=1&category=Greedy&sortBy=submissions
+# =============================================================================
+# =============================================================================
 # 313	Largest number possible	https://www.geeksforgeeks.org/problems/largest-number-possible5028/1?page=1&category=Greedy&sortBy=submissions
+# =============================================================================
+# =============================================================================
 # 314	Lemonade change	https://www.geeksforgeeks.org/problems/lemonade-change/1?page=2&category=Greedy&sortBy=submissions
+# =============================================================================
+# =============================================================================
 # 315	Activity selection prob(Super imp)	https://www.geeksforgeeks.org/problems/activity-selection-1587115620/1?page=1&category=Greedy&sortBy=submissions
+# =============================================================================
+# =============================================================================
 # 316	N meetings in a room(Super imp)	https://www.geeksforgeeks.org/problems/n-meetings-in-one-room-1587115620/1?page=1&category=Greedy&sortBy=submissions
+# =============================================================================
+# =============================================================================
 # 317	Minimum platforms 	https://www.geeksforgeeks.org/problems/minimum-platforms-1587115620/1?page=1&category=Greedy&sortBy=submissions
+# =============================================================================
+# =============================================================================
 # 318	Minimize the height 	https://www.geeksforgeeks.org/problems/minimize-the-heights-i/1?page=2&category=Greedy&sortBy=submissions
+# =============================================================================
+# =============================================================================
 # 319	Minimum number of coins using greedy(This can be solved using naive, DP also but make sure to use greedy here)	https://www.geeksforgeeks.org/problems/-minimum-number-of-coins4426/1?page=1&category=Greedy&sortBy=submissions
+# =============================================================================
+# =============================================================================
 # 320	Job sequencing prob	https://www.geeksforgeeks.org/problems/job-sequencing-problem-1587115620/1?page=1&category=Greedy&sortBy=submissions
+# =============================================================================
+# =============================================================================
 # 321	Fractional knapsack	https://www.geeksforgeeks.org/problems/fractional-knapsack-1587115620/1?page=1&category=Greedy&sortBy=submissions
+# =============================================================================
+# =============================================================================
 # 322	Jump game	https://www.geeksforgeeks.org/problems/jump-game/1?page=2&category=Greedy&sortBy=submissions
+# =============================================================================
+# =============================================================================
 # 323	Insert interval	https://leetcode.com/problems/insert-interval/
+# =============================================================================
+# =============================================================================
 # 324	Merge interval	https://leetcode.com/problems/merge-intervals/
+# =============================================================================
+# =============================================================================
 # 325	Non overlapping interval	https://leetcode.com/problems/non-overlapping-intervals/
+# =============================================================================
+# =============================================================================
 # 326	Minimum cost of ropes	https://practice.geeksforgeeks.org/problems/minimum-cost-of-ropes-1587115620/1
+# =============================================================================
+# =============================================================================
 # 327	Partition labels	https://leetcode.com/problems/partition-labels/
+# =============================================================================
+# =============================================================================
 # 328	Cinema seat allocation	https://leetcode.com/problems/cinema-seat-allocation/
+# =============================================================================
+# =============================================================================
 # 329	Max length chain	https://www.geeksforgeeks.org/problems/max-length-chain/1?page=1&category=Greedy&sortBy=submissions
+# =============================================================================
+# =============================================================================
 # 330	Valid parantheses - Minimum	https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/
+# =============================================================================
+# =============================================================================
 # 331	Shortest job first	https://practice.geeksforgeeks.org/problems/shortest-job-first/1
+# =============================================================================
+# =============================================================================
 # 332	Least recently used	 https://practice.geeksforgeeks.org/problems/lru-cache/1?utm_source=youtube&utm_medium=collab_fraz_websitelink&utm_campaign=LRU+Cache
 # =============================================================================
 # =============================================================================
