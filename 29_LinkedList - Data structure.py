cb7b9e26a78cc6cab5ce61382b454f5fabc7b33b
RB <60027406+RamyaVin@users.noreply.github.com>
2024-02-06 19:40:00 +0530

    Update 29_LinkedList - Data structure.py

--- 29_LinkedList - Data structure.py
+++ 29_LinkedList - Data structure.py
@@ -1,45 +1,125 @@
 # =============================================================================
 # =============================================================================
 # 29_LinkedList - Data structure		
+# =============================================================================
+# =============================================================================
 # 384	Best time to buy and sell stock	https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+# =============================================================================
+# =============================================================================
 # 385	Best time to buy and sell stock II(Once you solve the above prob, all probs in this pattern are extended versions, so you can solve by yourself)	https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+# =============================================================================
+# =============================================================================
 # 386	Best time to buy and sell stock III	https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/
+# =============================================================================
+# =============================================================================
 # 387	Best time to buy and sell stock IV	https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv/
+# =============================================================================
+# =============================================================================
 # 388	Best time to buy and sell stock with cooldown	https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
+# =============================================================================
+# =============================================================================
 # 389	Best time to buy and sell stock with transaction fee	https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/
+# =============================================================================
+# =============================================================================
 # Stack - Data structure		
+# =============================================================================
+# =============================================================================
 # 390	Matrix chain multiplication(Quite tricky pattern, every prob is diff and teaches something so do solve all)	https://www.geeksforgeeks.org/problems/matrix-chain-multiplication0303/1?page=2&category=Dynamic%20Programming&sortBy=submissions
+# =============================================================================
+# =============================================================================
 # 391	Minimum cost to cut stick	https://leetcode.com/problems/minimum-cost-to-cut-a-stick/
+# =============================================================================
+# =============================================================================
 # 392	Burst balloons	https://www.geeksforgeeks.org/problems/burst-balloons/1?page=8&category=Dynamic%20Programming&sortBy=submissions
+# =============================================================================
+# =============================================================================
 # 393	Boolean Parenthesization	https://www.geeksforgeeks.org/problems/boolean-parenthesization5610/1?page=3&category=Dynamic%20Programming&sortBy=submissions
+# =============================================================================
+# =============================================================================
 # 394	Palindrom partitioning	https://www.geeksforgeeks.org/problems/palindromic-patitioning4845/1?page=2&category=Dynamic%20Programming&sortBy=submissions
+# =============================================================================
+# =============================================================================
 # 395	Partition array for max sum	https://leetcode.com/problems/partition-array-for-maximum-sum/
+# =============================================================================
+# =============================================================================
 # 396	Max rectangles area with all 1s	https://www.geeksforgeeks.org/problems/max-rectangle/1?page=3&category=Dynamic%20Programming&sortBy=submissions
+# =============================================================================
+# =============================================================================
 # 397	Count square submatrices	https://leetcode.com/problems/count-square-submatrices-with-all-ones/
+# =============================================================================
+# =============================================================================
 # Queue - Data structure		
+# =============================================================================
+# =============================================================================
 # Sliding Window - Technique  (Dont skip this topic, I personally have been asked this topic in many interviews	GeekForGeeks has very limited probs in this topics, so use leetcode for this even if you practice on gfg generally	
+# =============================================================================
+# =============================================================================
 # 398	What is heap? Understand the data structure	
+# =============================================================================
+# =============================================================================
 # 399	Min heap implementation	Min Heap Implementation - Coding Ninjas
+# =============================================================================
+# =============================================================================
 # 400	Max heap implementation	https://www.gatevidyalay.com/tag/max-heap-questions/
+# =============================================================================
+# =============================================================================
 # 401	Convert min heap to max heap	https://www.geeksforgeeks.org/problems/convert-min-heap-to-max-heap-1666385109/1
+# =============================================================================
+# =============================================================================
 # Trees - Data structure		
+# =============================================================================
+# =============================================================================
 # 402	kth largest in array	https://www.geeksforgeeks.org/problems/k-largest-elements4206/1?page=1&category=Heap&sortBy=submissions
+# =============================================================================
+# =============================================================================
 # 403	kth smallest in array	
+# =============================================================================
+# =============================================================================
 # 404	Top k frequent elements	https://leetcode.com/problems/top-k-frequent-elements/
+# =============================================================================
+# =============================================================================
 # 405	Top k frequent words	https://leetcode.com/problems/top-k-frequent-words
+# =============================================================================
+# =============================================================================
 # 406	Frequency sort	https://leetcode.com/problems/sort-characters-by-frequency/
+# =============================================================================
+# =============================================================================
 # 407	kth largest num in streams	https://www.geeksforgeeks.org/problems/kth-largest-element-in-a-stream2220/1?page=1&category=Heap&sortBy=submissions
+# =============================================================================
+# =============================================================================
 # 408	Reorganize string	https://leetcode.com/problems/reorganize-string/
+# =============================================================================
+# =============================================================================
 # 409	Reorganize string k distance apart	https://leetcode.com/problems/rearrange-string-k-distance-apart
+# =============================================================================
+# =============================================================================
 # 410	kth smallest sum of matrix	https://leetcode.com/problems/find-the-kth-smallest-sum-of-a-matrix-with-sorted-rows/
+# =============================================================================
+# =============================================================================
 # 411	ugly num II	https://leetcode.com/problems/ugly-number-ii/
+# =============================================================================
+# =============================================================================
 # Backtracking & Recursion advanced - Technique	This is super imp topic before moving further!! Draw the recursion tree to understand the flow as the first step, this will help in visualization of the flow	
+# =============================================================================
+# =============================================================================
 # 412	Merge k sorted array	https://practice.geeksforgeeks.org/problems/merge-k-sorted-arrays/1
+# =============================================================================
+# =============================================================================
 # 413	k pairs with smallest sum	https://leetcode.com/problems/find-k-pairs-with-smallest-sums/
+# =============================================================================
+# =============================================================================
 # 414	k smallest nums in M sorted list	https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/
+# =============================================================================
+# =============================================================================
 # Graphs - Data structure	Most of the probs in this topics can be solved once you learn first couple of probs in each pattern of this topics	
+# =============================================================================
+# =============================================================================
 # 415	Find median from data stream	https://www.geeksforgeeks.org/problems/find-median-in-a-stream-1587115620/1?page=1&category=Heap&sortBy=submissions
+# =============================================================================
+# =============================================================================
 # 416	Sliding window median	https://leetcode.com/problems/sliding-window-median/
+# =============================================================================
+# =============================================================================
 # 417	Maximum capital/IPO	https://leetcode.com/problems/ipo/
 # =============================================================================
 # =============================================================================
