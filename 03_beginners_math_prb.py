56f0bdf186c800de016fc4e845c020fb6cf5b6db
RB <60027406+RamyaVin@users.noreply.github.com>
2024-02-06 07:56:31 +0530

    Update 03_beginners_math_prb.py

--- 03_beginners_math_prb.py
+++ 03_beginners_math_prb.py
@@ -10,6 +10,18 @@ print(Solution.evenOdd(4))
 # =============================================================================
 # Find last digit in a number
 class Solution:
+    def getLastDigit(self, a, b):
+        if b=='0' or a=='1':
+            return 1
+        elif b=='1':
+            return a
+        b = int(b)%4
+        if b==0:
+            b=4
+        a=int(a[-1])
+        return (a**b)%10
+#################
+class Solution:
     def lastDigit(x):
         return abs(x%10)
         
@@ -49,6 +61,16 @@ Solution.reverse(345345601)
 # =============================================================================
 # Find power of a number
 #12**21/(10**9+7)
+class Solution:
+    m = (10**9  + 7)
+    def power(self,N,R):
+        if R == 0:
+            return 1
+        elif R&1 :
+            return (N*((self.power(N,R//2))**(2)))%(self.m)
+        else: 
+            return ((self.power(N,R//2))**(2))%(self.m)
+###############
 class Solution():
     def powerOfNumber( x):
         if x > 2 ** 31 - 1 or x < -(2 ** 31):
@@ -66,10 +88,7 @@ class Solution():
         mod=10**9 + 7
         print(temp,result,mod)
         return print(pow(temp,result,mod))
-    
-    
 Solution.powerOfNumber(12)
-
 # =============================================================================
 # =============================================================================
 # GCD
@@ -109,17 +128,19 @@ Solution.sum_divs(45)
 # =============================================================================
 # =============================================================================
 # Prime number(Try solving by yourself)
-class Solution():
-    def prime(n):
-        flag=0
-        for i in range(2, n**(1//2), 1 ): 
-            if (n%i==0): 
-                flag=1               
-                break
-        if flag==1: print("Not Prime")
-        else: print("Prime")
-        return
-    
+class Solution:
+    def isPrime (self, n):
+        # code here
+        count=0
+        for i in range(1,int(n**0.5)+1):
+            if n%i==0:
+                count+=1
+                if n/i!=i:
+                    count+=1
+        if count==2:
+            return 1
+        else:
+            return 0
 Solution.prime(139)
 # =============================================================================
 # =============================================================================
@@ -131,8 +152,10 @@ class Solution():
         for i in range(1, n): 
             result=result+(n%10)**(digits)
             n=n//10
-        return result
-    
+        if n==0:
+            return "Yes"
+        else: return "No"
+            
 Solution.armstrong(123)
 # =============================================================================
 # =============================================================================
@@ -141,9 +164,9 @@ class Solution():
     def palindrome(n):
         x=str(n)
         if x==x[::-1]:
-            return "Palindrome"
+            return "Yes"
         else:
-            return "Not Palindrome" 
+            return "No" 
     
 Solution.palindrome(100)
 # also palindrom of negative number 
@@ -160,19 +183,21 @@ class Solution(object):
             div*=10
 
         while x:
-            if x // div != x % 10:return False 
+            if x // div != x % 10:return "No" 
             x = (x % div) // 10 
             div = div/100
 
-        return True
+        return "Yes"
 # =============================================================================
 # =============================================================================
 # Square root of a number(Try to first figure out algo to solve this)
-class Solution():
-    def squareRoot(n):
+import math
+#Complete this function
+class Solution:
+    def floorSqrt(self, n): 
         if n>0: flag=1
         else: flag=-1
-        return flag*(abs(n)**(1/2) )
+        return math.floor(flag*(n**(1/2)))
     
 Solution.squareRoot(-100)
 # =============================================================================
@@ -180,14 +205,16 @@ Solution.squareRoot(-100)
 # Perfect number
 class Solution():
     def perfectNumber(n):
-        total=0
-        for i in range(1,n**(1//2))+1):
+        total=1
+        if n==0 or n==1: return 0
+        for i in range(2,int(n**0.5)+1):
             if n%i==0: 
                 total=total+i
+                total+=n//i
+        print(total,n)
         if total == n:
-            return "Perfect number" 
+            return 1 
         else: 
-            return "Not perfect number"
-    
+            return 0
 Solution.perfectNumber(61)
 # =============================================================================
